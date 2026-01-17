52274ebd9323a0db84ad2f45a682146f69cbc63a
Kali <manishpc@localhost>
2026-01-17 19:21:04 +0530

    hrms

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,6 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "https://hrms-umber-nine.vercel.app"
+    "https://hrms-umber-nine.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
